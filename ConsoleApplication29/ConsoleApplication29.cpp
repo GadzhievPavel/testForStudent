f6b643901da4a5b78e0124e3b24f730a0fcfa55a
GadzhievPavel <you@example.com>
2021-03-29 19:51:05 +0300

    add Hello world *3

--- ConsoleApplication29/ConsoleApplication29.cpp
+++ ConsoleApplication29/ConsoleApplication29.cpp
@@ -6,6 +6,9 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
